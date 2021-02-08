43c335981735e4269b1ebf49c7c4caeab217b6ef
Morten <mtnielsen89@gmail.com>
2021-02-08 13:02:28 +0100

    Deleted app.test - this is not relevant now

--- App.test.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render, waitFor } from "@testing-library/react-native";
-import App from "./App";
-
-// TODO: Make a "Happy Path" test for the user journey
-test("Ensure that navigation is available", async () => {});
